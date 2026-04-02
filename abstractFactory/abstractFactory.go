2b67a5652c28017c5dae60ee3d7e8d0b983b1806
Shreyansh Tiwari <Shreyansh.11906963@lpu.in>
2026-04-02 22:19:34 +0530

    added abstract factory code

---
+++ abstractFactory/abstractFactory.go
@@ -0,0 +1,59 @@
+package abstractFactory
+
+import "fmt"
+
+// dark theme -> dark button and check box objects wil be created
+
+// light theme -> dark button and check box objects wil be created
+
+type Button interface {
+	Render()
+}
+type CheckBox interface {
+	Render()
+}
+
+type LightButton struct{}
+type LightCheckBox struct{}
+
+func (l LightButton) Render() {
+	fmt.Printf("Rendering light button")
+}
+func (l LightCheckBox) Render() {
+	fmt.Printf("Rendering light checkbox")
+}
+
+type DarkButton struct{}
+type DarkCheckBox struct{}
+
+func (d DarkButton) Render() {
+	fmt.Printf("Rendering dark button")
+}
+func (d DarkCheckBox) Render() {
+	fmt.Printf("Rendering dark checkbox")
+}
+
+type UIFactorInterface interface {
+	CreateButton() Button
+	CreateCheckBox() CheckBox
+}
+
+type LightTheme struct{}
+
+func (l LightTheme) CreateButton() Button {
+	return LightButton{}
+}
+
+func (l LightTheme) CreateCheckBox() CheckBox {
+	return LightCheckBox{}
+}
+
+type DarkTheme struct{}
+
+func (d DarkTheme) CreateButton() Button {
+	return DarkButton{}
+}
+
+func (d DarkTheme) CreateCheckBox() CheckBox {
+	return DarkCheckBox{}
+}
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"design-patterns-go/abstractFactory"
 	"design-patterns-go/constants"
 	"design-patterns-go/factory"
 	"fmt"
 )
 
-func main() {
+func factoryPatternDriver() {
 	//object using factory
 	smsObj, err := factory.Create(constants.Sms)
 	if err != nil {
@@ -20,5 +21,25 @@ func main() {
 		fmt.Print("Something went wrong!")
 	}
 	emailObj.Send("shreyyy@gmail.com", "Heyyy!")
+}
 
+func abstractFactoryPatternDriver() {
+	var factory abstractFactory.UIFactorInterface
+	darkTheme := true
+	if darkTheme {
+		factory = abstractFactory.DarkTheme{}
+	} else {
+		factory = abstractFactory.LightTheme{}
+	}
+
+	button := factory.CreateButton()
+	checkbox := factory.CreateCheckBox()
+
+	button.Render()
+	checkbox.Render()
+}
+
+func main() {
+	// factoryPatternDriver()
+	abstractFactoryPatternDriver()
 }
